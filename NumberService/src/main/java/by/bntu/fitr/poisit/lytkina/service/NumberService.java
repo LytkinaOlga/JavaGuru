15b7815ef2e1211923b5ad340003150186dde9b9
LytkinaOlga <65194987+LytkinaOlga@users.noreply.github.com>
2020-09-16 19:17:54 +0300

    create project NumberService with tests

---
+++ NumberService/src/main/java/by/bntu/fitr/poisit/lytkina/service/NumberService.java
@@ -0,0 +1,17 @@
+package by.bntu.fitr.poisit.lytkina.service;
+
+public class NumberService {
+    public int rangeSum(int start, int finish){
+        int sum = 0;
+        if (start < finish){
+            for (int i = start; i <= finish; i++){
+                sum += i;
+            }
+        }else if (start > finish){
+            for (int i = finish; i <= start; i++){
+                sum += i;
+            }
+        }else sum = 0;
+        return sum;
+    }
+}
